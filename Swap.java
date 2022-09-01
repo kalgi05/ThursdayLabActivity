79fad98ba18217195f9ab377bd5792c63b904c49
kalgi05 <109600775+kalgi05@users.noreply.github.com>
2022-09-01 18:28:09 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,17 @@
+//swap number example
+
+package ThursdayLabTest;
+
+public class SwapNumber {
+	
+			public static void main(String[] args) {
+				int a=10,b=20;
+				System.out.println("before swapping value of a and b is:"+a+","+b);
+				a=a+b;
+				b=a-b;
+				a=a-b;
+				System.out.println("after swppingg value of a and b is:"+a+","+b);
+		
+	}
+
+}
